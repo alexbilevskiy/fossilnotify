e44362d76012837b6413e73f0f03051c298f1a97
Alex <thealexsb@gmail.com>
2026-01-02 17:28:53 +0300

    update log

--- app/src/main/java/nodomain/freeyourgadget/fossilnotify/service/ui/UiBroadcastReceiver.kt
+++ app/src/main/java/nodomain/freeyourgadget/fossilnotify/service/ui/UiBroadcastReceiver.kt
@@ -3,13 +3,10 @@ package nodomain.freeyourgadget.fossilnotify.service.ui
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import nodomain.freeyourgadget.fossilnotify.service.gb.GBService
 import nodomain.freeyourgadget.fossilnotify.ui.view_model.ViewModel
 
 class UiBroadcastReceiver(
-    private val viewModel: ViewModel,
-    private val gbService: GBService
+    private val viewModel: ViewModel
 ): BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -18,7 +15,7 @@ class UiBroadcastReceiver(
         val upperText1 = intent?.getStringExtra("upper_text1")
         val lowerText1 = intent?.getStringExtra("lower_text1")
 
-        viewModel.updateText(String.format("NOTIF: %s, %s, %s, %s", upperText0, lowerText0, upperText1, lowerText1))
+        viewModel.updateText(String.format("NOTIF: `%s`, `%s`, `%s`, `%s`", upperText0, lowerText0, upperText1, lowerText1))
     }
 
 }
