bc490cb99c28a082d2208492407942cffafa3011
Alex <thealexsb@gmail.com>
2026-01-04 12:13:22 +0300

    remove submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,25 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven(url = "https://jitpack.io")
     }
-    versionCatalogs {
-        create("libpebble") {
-            from(files("libs/libpebble3/gradle/libs.versions.toml"))
-        }
-    }
 }
 rootProject.name = "FossilNotify"
 include(":app")
-
-
-// TODO: comment these modules in libs/libpebble3/settings.gradle.kts - they are core app components
-//include(":composeApp")
-//include(":pebble")
-//include(":util")
-//include(":experimental")
-
-//includeBuild("libs/libpebble3") {
-//    dependencySubstitution {
-//        substitute(module("io.rebble.libpebblecommon:libpebble3"))
-//            .using(project(":libpebble3"))
-//    }
-//}
